04a5de0eab438e13fcf5577382ec97e502cbf3bc
Japheth <jkarani171@gmail.com>
2024-05-04 03:50:39 -0600

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,14 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
+
 {/* write an <About> component here */}
 
 
@@ -24,7 +32,8 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      { <Home /> }
+      { <About /> }
     </div>
   );
 }
